6a734a48815fb95b55a9038468e110c8df48e61a
Kevin Huang <huangyf9883@gmail.com>
2026-07-21 02:13:20 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,8 +42,7 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'cloudflare-pages',
     prerender: {
-      routes: ['/'],
-      crawlLinks: true,
+      routes: ['/', '/about'],
     },
   },
 
